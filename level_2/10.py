fc3e1e11bf864f8da5bdcd15ff90e635e55e30c8
AlexandraPoturaeva <av-poturaeva@yandex.ru>
2023-09-21 10:05:35 +0300

    fix indentation

--- level_2/10.py
+++ level_2/10.py
@@ -2,9 +2,9 @@ from constants import ___
 
 
 def is_point_in_square(
-        point: tuple[int, int],
-        left_upper_corner: tuple[int, int],
-        right_bottom_corner: tuple[int, int],
+    point: tuple[int, int],
+    left_upper_corner: tuple[int, int],
+    right_bottom_corner: tuple[int, int],
 ) -> bool:
     pass
 
